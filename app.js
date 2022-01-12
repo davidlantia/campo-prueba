84125508f808fd0f2b5dda118c57880ac5577478
David <david.granados@lantia.com>
2022-01-12 12:30:57 +0100

    Cambios

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080;
 
-//const server = http.createServer((req,res)=>{
-http.createServer((req,res)=>{
-    
-    //Indica que retornando un archivo de texto
-    //res.writeHead(200,{'Content-Type':'text/plain'});
-    res.setHeader('Content-Disposition','attachment; filename=lista.csv');
-    res.writeHead(200,{'Content-Type':'application/csv'});
-    //res.write('404 | Page not found');
-    res.write('id,nombre\n');
-    res.write('1,Fernando\n');
-    res.write('2,Maria\n');
-    res.write('3,Juan\n');
-    res.write('4,Pedro\n');
-    res.end();
-})
-.listen(8080);
-console.log('Escuchando el puerto', 8080);
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+app.get('*', (req, res) => {
+res.sendFile ( __dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
